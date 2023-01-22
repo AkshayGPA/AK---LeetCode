4bb68ec94c57455dd6a7d76bcfdaa592ba5e5be7
akshay_gaikwad <akshay240695@gmail.com>
2023-01-22 14:46:59 +0530

    Addind all problems to branch

---
+++ BinarySearch.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class BinarySearch {
+    public static void main (String [] args) {
+        // Take input using string builder
+        
+    }
+
+    private boolean isAvailable(int arr[], int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        int mid = (start+end)/2;
+
+        while (start <= end){
+
+            if(arr[mid] == target) {
+                return true;
+            }
+            else if (arr[mid] > target){
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        
+    }
+}
+
+arr = [3]
